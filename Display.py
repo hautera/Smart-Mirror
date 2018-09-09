4f707d873f5ea9f546e13329dc2115044616d165
hautera <hautea@uw.edu>
2018-09-08 21:57:12 -0700

    Split App into display, requests, and an app delegate, also added wind

---
+++ Display.py
@@ -0,0 +1,311 @@
+import datetime as dt
+import time
+import configparser
+import PIL.Image
+import PIL.ImageTk
+from tkinter import *
+
+
+config = configparser.ConfigParser()
+config.read('Settings')
+
+GOOGLE_DATE_FORMAT = r'%Y-%m-%d'
+GOOGLE_TIME_FORMAT = r'T%H:%M'
+
+
+time_format = '%H:%M' if  config['SETTINGS']['Twenty Four Hour Clock'] == 'True' else '%I:%M %p'
+
+date_format = config['SETTINGS']['Date Format'].replace( "M", r'%b').replace("D", r'%d').replace("Y", '%y')
+
+weather_lang = config['SETTINGS']['Language']
+weather_unit = config['SETTINGS']['Country Code']
+
+XLARGE_TEXT_SIZE = 94
+LARGE_TEXT_SIZE = 48
+MEDIUM_TEXT_SIZE = 28
+SMALL_TEXT_SIZE = 18
+
+icon_lookup = {
+    'clear-day': "assets/Sun.png",  # clear sky day
+    'wind': "assets/Wind.png",   #wind
+    'cloudy': "assets/Cloud.png",  # cloudy day
+    'partly-cloudy-day': "assets/PartlySunny.png",  # partly cloudy day
+    'rain': "assets/Rain.png",  # rain day
+    'snow': "assets/Snow.png",  # snow day
+    'snow-thin': "assets/Snow.png",  # sleet day
+    'fog': "assets/Haze.png",  # fog day
+    'clear-night': "assets/Moon.png",  # clear sky night
+    'partly-cloudy-night': "assets/PartlyMoon.png",  # scattered clouds night
+    'thunderstorm': "assets/Storm.png",  # thunderstorm
+    'tornado': "assests/Tornado.png",    # tornado
+    'hail': "assests/Hail.png"  # hail
+}
+
+class Clock(Frame):
+    # TODO analog clock ?
+    def __init__(self, parent, *args, **kwargs):
+        Frame.__init__(self, parent, bg='black')
+        # initialize time label
+        self.timeLbl = Label(self, font=('Helvetica', LARGE_TEXT_SIZE), fg="white", bg="black")
+        self.timeLbl.pack(side=TOP, anchor=E)
+
+        # initialize day of week
+        self.dayOWLbl = Label(self, text='', font=('Helvetica', SMALL_TEXT_SIZE), fg="white", bg="black")
+        self.dayOWLbl.pack(side=TOP, anchor=E)
+
+        # initialize date label
+        self.dateLbl = Label(self, text='', font=('Helvetica', SMALL_TEXT_SIZE), fg="white", bg="black")
+        self.dateLbl.pack(side=TOP, anchor=E)
+
+
+    def update( self ):
+        new_date_time = dt.datetime.now()
+        day_of_week = new_date_time.strftime('%A')
+        date = new_date_time.strftime(date_format)
+        time = new_date_time.strftime(time_format)
+        self.timeLbl.config(text=time)
+        self.dayOWLbl.config(text=day_of_week)
+        self.dateLbl.config(text=date)
+
+class Weather(Frame):
+
+    def __init__(self, parent, *args, **kwargs):
+        Frame.__init__(self, parent, bg='black')
+
+        self.windFrm = Frame(self, bg='black')
+        self.windFrm.pack( side=TOP, anchor=W )
+
+        self.degreeFrm = Frame(self, bg="black")
+        self.degreeFrm.pack(side=TOP, anchor=W)
+
+        self.temperatureLbl = Label(self.degreeFrm, font=('Helvetica', XLARGE_TEXT_SIZE), fg="white", bg="black")
+        self.temperatureLbl.pack(side=LEFT, anchor=N)
+
+        self.iconLbl = Label(self.degreeFrm, bg="black")
+        self.iconLbl.pack(side=LEFT, anchor=N, padx=20)
+
+        self.windLbl = Label( self.windFrm, fg='white', bg='black' )
+        self.windLbl.pack( side=BOTTOM, anchor=N, padx=20 )
+
+        self.wind_barb_label = Label(self.windFrm, bg='black')
+        self.wind_barb_label.pack(side=LEFT, anchor=N, padx=20)
+
+        self.currentlyLbl = Label(self, font=('Helvetica', MEDIUM_TEXT_SIZE), fg="white", bg="black")
+        self.currentlyLbl.pack(side=TOP, anchor=W)
+
+        self.forecastLbl = Label(self, font=('Helvetica', SMALL_TEXT_SIZE), fg="white", bg="black")
+        self.forecastLbl.pack(side=TOP, anchor=W)
+
+        self.locationLbl = Label(self, font=('Helvetica', SMALL_TEXT_SIZE), fg="white", bg="black")
+        self.locationLbl.pack(side=TOP, anchor=W)
+
+    def update(self, weather_obj):
+        degree_sign= u'\N{DEGREE SIGN}'
+        temperature = "%s%s" % (str(int(weather_obj['currently']['temperature'])), degree_sign)
+
+        # Get weather in english description
+        currently = weather_obj['currently']['summary']
+        forecast = weather_obj["hourly"]["summary"]
+        # TODO FORMAT THE FORECAST NICELY SUCH THAT THERE IS NO OVERLAP :)
+
+        # Lil weather icon
+        icon_id = weather_obj['currently']['icon']
+        icon = icon_lookup['rain'] if not icon_id in icon_lookup else icon_lookup[icon_id]
+        image = PIL.Image.open(icon)
+        image = image.resize((100, 100), PIL.Image.ANTIALIAS)
+        image = image.convert('RGB')
+        photo = PIL.ImageTk.PhotoImage(image)
+
+        # Get that there wind
+        wind_speed = int(weather_obj['currently']['windSpeed'])
+        wind_gust = int(weather_obj['currently']['windGust'])
+        wind_direction = int(weather_obj['currently']['windBearing'])
+
+        # Cardinal direction of the wind (There's gotta be a better way to do this )
+        if wind_direction in range(0,30) or wind_direction in range(330, 360):
+            cardinal_wind_direction = "N"
+        elif wind_direction in range(30,60):
+            cardinal_wind_direction = "NE"
+        elif wind_direction in range(60, 120):
+            cardinal_wind_direction = "E"
+        elif wind_direction in range(120, 150):
+            cardinal_wind_direction = "SE"
+        elif wind_direction in range(150, 210):
+            cardinal_wind_direction = "S"
+        elif wind_direction in range(210, 240):
+            cardinal_wind_direction = "SW"
+        elif wind_direction in range(240, 300):
+            cardinal_wind_direction = "W"
+        else:
+            cardinal_wind_direction = "NW"
+
+        wind_str = "%dknts %s, with gusts of %dknts" % (wind_speed, cardinal_wind_direction, wind_gust )
+
+        # Rounds the windspeed to the nearest 5 knts
+        wind_est = wind_speed  - (wind_speed % 5)
+
+        # Gets the wind barb image ready
+        wind_image = PIL.Image.open( "assets/PNG/%dknts.png" % wind_est ) if wind_est <= 30 else PIL.Image.open( "assets/PNG/panic.png" )
+        wind_image = wind_image.rotate( wind_direction ).resize((100, 100), PIL.Image.ANTIALIAS).convert('RGB')
+        wind_image = PIL.ImageTk.PhotoImage( wind_image )
+
+        self.iconLbl.config(image=photo)
+        self.iconLbl.image = photo
+
+        self.wind_barb_label.config(image=wind_image)
+        self.wind_barb_label.image = wind_image
+
+        self.currentlyLbl.config(text=currently)
+        self.forecastLbl.config(text=forecast)
+        self.temperatureLbl.config(text=temperature)
+        self.windLbl.config(text=wind_str)
+
+
+    @staticmethod
+    def convert_kelvin_to_fahrenheit(kelvin_temp):
+        return 1.8 * (kelvin_temp - 273) + 32
+
+class News(Frame):
+
+    class NewsHeadline(Frame):
+        def __init__(self, parent, event_name=""):
+            Frame.__init__(self, parent, bg='black')
+
+            image = PIL.Image.open("assets/Newspaper.png")
+            image = image.resize((25, 25), PIL.Image.ANTIALIAS)
+            image = image.convert('RGB')
+            photo = PIL.ImageTk.PhotoImage(image)
+
+            self.iconLbl = Label(self, bg='black', image=photo)
+            self.iconLbl.pack(side=LEFT, anchor=N)
+
+            self.eventNameLbl = Label(self, text=event_name, font=('Helvetica', SMALL_TEXT_SIZE), fg="white", bg="black")
+            self.eventNameLbl.pack(side=LEFT, anchor=N)
+
+    def __init__(self, parent, *args, **kwargs):
+        Frame.__init__(self, parent, *args, **kwargs)
+        self.config(bg='black')
+
+        self.newsLbl = Label(self, text='News', font=('Helvetica', MEDIUM_TEXT_SIZE), fg="white", bg="black")
+        self.newsLbl.pack(side=TOP, anchor=W)
+
+        self.headlinesContainer = Frame(self, bg="black")
+        self.headlinesContainer.pack(side=TOP)
+
+
+    def update(self, feed):
+        for widget in self.headlinesContainer.winfo_children():
+            widget.destroy()
+
+        for post in feed.entries[0:5]:
+            headline = self.NewsHeadline(self.headlinesContainer, post.title)
+            headline.pack(side=TOP, anchor=W)
+
+class Calendar(Frame):
+    # TODO: Date/ time of event
+    class CalendarEvent(Frame):
+        def __init__(self, parent, event_name, event_start_date):
+            Frame.__init__(self, parent, bg='black')
+            dt_format = date_format
+            if 'dateTime' in event_start_date.keys():
+                # Formats the date and time nicely :)
+                dt_format = date_format + "\n" + time_format
+                date = dt.datetime.strptime( event_start_date['dateTime'][:-9], GOOGLE_DATE_FORMAT + GOOGLE_TIME_FORMAT )
+            else:
+                date = dt.datetime.strptime(event_start_date['date'], GOOGLE_DATE_FORMAT)
+
+            self.eventNameLbl = Label(self, text=event_name, font=('Helvetica', SMALL_TEXT_SIZE), fg="white", bg="black")
+            self.eventDateLbl = Label( self, text=date.strftime(dt_format), font=('Helvetica', SMALL_TEXT_SIZE), fg='white', bg='black')
+
+            self.eventNameLbl.pack(side=TOP, anchor=E)
+            self.eventDateLbl.pack(side=TOP, anchor=E)
+
+    def __init__(self, parent, *args, **kwargs):
+        Frame.__init__(self, parent, bg='black')
+        self.calendarLbl = Label(self, text='Calendar Events', font=('Helvetica', MEDIUM_TEXT_SIZE), fg="white", bg="black")
+        self.calendarLbl.pack(side=TOP, anchor=E)
+        self.calendarEventContainer = Frame(self, bg='black')
+        self.calendarEventContainer.pack(side=TOP, anchor=E)
+
+    def update(self, events):
+        for widget in  self.calendarEventContainer.winfo_children():
+            widget.destroy()
+
+        for event in events:
+            calendar_event = self.CalendarEvent(self.calendarEventContainer, event["summary"], event['start'])
+            calendar_event.pack(side=TOP, anchor=E)
+
+class FullscreenWindow:
+
+    def __init__(self):
+        self.tk = Tk()
+        self.tk.configure(background='black')
+        self.topFrame = Frame(self.tk, background = 'black')
+        self.topFrame.pack(side = TOP, fill=BOTH, expand = YES)
+
+        self.bottomFrame = Frame(self.tk, background = 'black')
+        self.bottomFrame.pack(side = BOTTOM, fill=BOTH, expand = YES)
+
+        self.is_full_screen = False
+        self.tk.bind("<Return>", self.toggle_fullscreen)
+        self.tk.bind("<Escape>", self.end_fullscreen)
+        self.tk.bind('s', self.show_widgets)
+        self.tk.bind('h', self.hide_widgets)
+        self.widgets_exist = {}
+        self.widgets = {}
+        # clock
+        self.widgets_exist['clock'] = config['DISPLAY OPTIONS']['Clock'] == 'True'
+        if self.widgets_exist['clock']:
+            self.widgets['clock'] = Clock(self.topFrame)
+
+        # weather
+        self.widgets_exist['weather'] = config["DISPLAY OPTIONS"]["Weather"] == 'True'
+        if self.widgets_exist['weather'] :
+            self.widgets['weather'] = Weather(self.topFrame)
+
+        # news ( I don't like the news )
+        self.widgets_exist['news'] = config["DISPLAY OPTIONS"]["News"] == "True"
+        if self.widgets_exist['news']:
+            self.widgets['news'] = News(self.bottomFrame)
+
+        # calender - removing for now
+        self.widgets_exist['calendar'] = config["DISPLAY OPTIONS"]["Calendar"] == "True"
+        if self.widgets_exist['calendar']:
+            self.widgets['calendar'] = Calendar(self.bottomFrame)
+
+        for widget in self.widgets.values():
+            widget.pack(side = LEFT, anchor=S, padx=100, pady=60)
+
+    def toggle_fullscreen(self, event=None):
+        self.is_full_screen = not self.is_full_screen  # Just toggling the boolean
+        self.tk.attributes("-fullscreen", self.is_full_screen)
+        return "break"
+
+    def end_fullscreen(self, event=None):
+        self.state = False
+        self.tk.attributes("-fullscreen", False)
+        return "break"
+
+    def hide_widgets(self, event=None):
+        for widget in self.widgets:
+            widget.pack_forget()
+
+    def show_widgets(self, event=None):
+        for widget in self.widgets:
+            widget.pack(side = LEFT, anchor=S, padx=100, pady=60)
+
+    def update_clock(self):
+        if self.widgets_exist['clock']:
+            self.widgets['clock'].update()
+
+    def update_news( self, feed ):
+        if self.widgets_exist['news']:
+            self.widgets['news'].update( feed )
+
+    def update_weather( self, data ):
+        if self.widgets_exist['weather']:
+            self.widgets['weather'].update( data )
+
+    def update_calendar( self, data):
+        if self.widgets_exist['calendar']:
+            self.widgets['calendar'].update( data )
---
+++ Requests.py
@@ -0,0 +1,73 @@
+import asyncio
+import aiohttp
+import json
+import configparser
+from googleapiclient.discovery import build
+from httplib2 import Http
+from oauth2client import file, client, tools
+import datetime as dt
+
+import feedparser
+
+config = configparser.ConfigParser()
+config.read('Settings')
+
+
+ip_api_token = config['SETTINGS']['IP Locater Api Code']
+
+weather_api_token = config['SETTINGS']['Weather Api Code']
+weather_lang = config['SETTINGS']['Language']
+weather_unit = config['SETTINGS']['Country Code']
+
+def main():
+    r = Request()
+    loop = asyncio.new_event_loop()
+    print( json.dumps(loop.run_until_complete( r.get_weather_data() )))
+
+
+class Request:
+
+    async def get( self, url ):
+        async with aiohttp.ClientSession(connector=aiohttp.TCPConnector(ssl=False)) as session:
+            async with session.get( url ) as response:
+                return await response.text()
+
+    async def get_weather_data(self):
+        location_data = await self.get_location_data()
+        weather_req_url = "https://api.darksky.net/forecast/%s/%s,%s?lang=%s&units=%s" % (weather_api_token, location_data['latitude'], location_data['longitude'], weather_lang, weather_unit)
+
+        return json.loads( await self.get( weather_req_url ))
+
+    async def get_location_data(self):
+        location_req_url = "http://api.ipstack.com/%s?access_key=%s&output=json" % (await self.get_ip(), ip_api_token )
+        r = await self.get(location_req_url)
+        return json.loads(r)
+
+    async def get_ip(self):
+        ip_url = "http://jsonip.com/"
+        req = await self.get(ip_url)
+        ip_json = json.loads(req)
+        return ip_json['ip']
+
+    async def get_news_feed( self ):
+        feed = await self.get("https://news.google.com/news/rss" )#% config['SETTINGS']['Country Code'])
+        return feedparser.parse( feed )
+
+    async def get_calendar_data( self ):
+        store = file.Storage('assets/token.json')
+        creds = store.get()
+
+        if not creds or creds.invalid:
+            flow = client.flow_from_clientsecrets('assets/credentials.json', CALENDAR_SCOPES )
+            creds = tools.run_flow(flow, store)
+        service = build( 'calendar', 'v3', http=creds.authorize(Http()))
+
+        # Call the calendar API
+        now = dt.datetime.utcnow().isoformat() + 'Z'
+        events_results = service.events().list(calendarId='primary', timeMin=now, maxResults=3, singleEvents=True, orderBy='startTime').execute()
+
+        return events_results.get( 'items', [] )
+
+
+if __name__ == '__main__':
+    main()
